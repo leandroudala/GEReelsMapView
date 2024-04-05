e948ea1c40a4e3da32d7cf1e6e62cfa785078e1d
Leandro Udala <leandro.correia@radixeng.com.br>
2024-04-05 18:09:28 -0300

    Initial Commit

---
+++ GEReelsMapView/GEReelsMapView.h
@@ -0,0 +1,12 @@
+//
+//  GEReelsMapView.h
+//  GEReelsMapView
+//
+//  Created by Leandro Udala Correia da Silva on 05/04/24.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GEReelsMapView : NSObject
+
+@end
---
+++ GEReelsMapView/GEReelsMapView.m
@@ -0,0 +1,12 @@
+//
+//  GEReelsMapView.m
+//  GEReelsMapView
+//
+//  Created by Leandro Udala Correia da Silva on 05/04/24.
+//
+
+#import "GEReelsMapView.h"
+
+@implementation GEReelsMapView
+
+@end
